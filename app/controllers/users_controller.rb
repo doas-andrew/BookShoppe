ed7a6d89c6e8a593143bb5b816bef27dc38080f1
github username <github email address>
2019-05-27 14:46:46 -0400

    login added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,29 @@ class UsersController < ApplicationController
 	before_action :set_user, only: [:show]
 
 	def index
-		@user = User.all.first
-		session[:current_user_id] = @user.id
-		render 'trades'
+		# @user = User.all.first
+		# session[:current_user_id] = @user.id
+		# render 'trades'
+		@users = User.all
 	end
 
 	def show
 		render 'user_profile'
+	end
+
+	def new
+    end
+
+    def create
+        User.create(first_name: params[:first_name],
+                       last_name: params[:last_name],
+                       email: params[:email],
+                       password: params[:password],
+					   password_confirmation: params[:password_confirmation],
+					   phone_number: params[:phone_number],
+					   address: params[:address],
+					   image_url: params[:image_url])
+		redirect_to :root
 	end
 
 	private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,19 @@ class User < ApplicationRecord
 	has_many :user_books
 	has_many :books, through: :user_books
 
+	VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	has_secure_password
+    before_save { self.email = email.downcase }
+    validates :first_name, presence: true, length: {maximum: 50}
+    validates :last_name, presence: true, length: {maximum: 50}
+    validates :email, presence: true, length: {maximum: 225},
+                      format: {with: VALID_EMAIL_REGEX},
+                      uniqueness: {case_sensitive: false}
+
+	def to_s
+		first_name + " " + last_name
+	end
+
 	def inc_trades
 		Trade.all.select {|t| t.recipient == self }
 	end
