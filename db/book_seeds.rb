06399b20ee0ae38e74a7f2a5583c075c06a96f8a
Andrew Allen <asallen67@gmail.com>
2019-05-31 09:48:03 -0400

    delete comment link added to chatbox

--- db/book_seeds.rb
+++ db/book_seeds.rb
@@ -2,7 +2,7 @@ Author.create(name: 'Czeslaw Milosz').books.create(title: 'Unattainable Earth')
 
 20.times do
 	author = Author.create(name: Faker::Book.author)
-	rand(3).times do
+	rand(4).times do
 		author.books.create(title: Faker::Book.title)
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ TradeBook.destroy_all
 Trade.destroy_all
 require_relative 'book_seeds' if Book.all.empty? && Author.all.empty?
 #...
-User.create(first_name: 'Andrew', last_name: 'Allen', username: 'ASA', email: '2@2.w', password: '123', phone_number: '1234567890', address: '900 Parsons Drive').user_books.create(book: Book.second)
+User.create(first_name: 'Andrew', last_name: 'Allen', username: 'ASAllen67', email: '2@2.w', password: '123', phone_number: '1234567890', address: '900 Parsons Drive').user_books.create(book: Book.second)
 User.create(first_name: 'Jaehyun', last_name: 'Park', username: 'AC2CIC', email: 'jae@test.com', password: '123', phone_number: '5551234567', address: '567 Roundtree Park').user_books << [UserBook.create(book: Book.third), UserBook.create(book: Book.third), UserBook.create(book: Book.last)]
 scott = User.create(first_name: 'Scott', last_name: 'Toney', username: 'batcat84', email: 'scott@test.com', password: '123', phone_number: '5556782222', address: 'West End Atlanta', avatar: "https://i.imgur.com/ha97zfB.jpg")
 
